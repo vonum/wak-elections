3051a2e32c81a800b37808af20de53c4c5a6928b
vonum <vonum.mk@gmail.com>
2022-08-08 14:34:36 +0200

    Basic server

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
+var cors = require("cors")
 const asyncHandler = require("express-async-handler");
 const axios = require("axios");
 
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_ADDRESS = process.env.VOTING_CONTRACT_ADDRESS;
 const web3 = createAlchemyWeb3(process.env.API_URL);
 
 const contract = require("./build/contracts/Voting.json");
 const votingContract = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS);
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 const fetchCandidates = async () => {
@@ -22,6 +24,10 @@ const fetchCandidates = async () => {
 
 app.get("/candidates", asyncHandler(async (req, res, next) => {
   const candidates = await fetchCandidates();
+  const data = await votingContract.methods.candidateVotes().call();
+  candidates.forEach((c, id) => {
+    c.votes = parseInt(data[id]);
+  });
   res.send(candidates);
 }));
 
