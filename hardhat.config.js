6802bc86d5935a83ab860353385ec982eca7e7d8
vonum <vonum.mk@gmail.com>
2022-08-08 14:33:46 +0200

    Switch to rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,10 @@ const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.15",
-   defaultNetwork: "goerli",
+   defaultNetwork: "rinkeby",
    networks: {
       hardhat: {},
-      goerli: {
+      rinkeby: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
